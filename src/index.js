bb2f663bab7efb0a10513fde57b7dccc345e8e59
Brandon <brandonstrash91@yahoo.com>
2019-07-06 13:37:03 -0400

    Set background color

--- src/index.js
+++ src/index.js
@@ -4,13 +4,21 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import UserStore from "./contexts/UserContext";
+import styled from "styled-components";
+const Body = styled.div`
+  background-color: #0c1d34;
+  color: #fff;
+`;
 
-ReactDOM.render(
-  <UserStore>
-    <App />
-  </UserStore>,
-  document.getElementById("root")
+const MainApp = () => (
+  <Body>
+    <UserStore>
+      <App />
+    </UserStore>
+  </Body>
 );
+
+ReactDOM.render(<MainApp />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
