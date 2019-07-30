aedb3103141acb406062a76184b8ba20af15c640
Brandon <brandonstrash91@yahoo.com>
2019-07-29 21:20:40 -0400

    fix input bug

--- src/components/hooks/useForm.js
+++ src/components/hooks/useForm.js
@@ -4,9 +4,11 @@ const useForm = initialState => {
   const [values, setValues] = useState(initialState);
 
   const handleChange = event => {
+    const name = event.target.name;
+    const value = event.target.value;
     setValues(values => ({
       ...values,
-      [event.target.name]: event.target.value
+      [name]: value
     }));
   };
 
